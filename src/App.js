0c4df734f0fcf14a3a24ab2aa497af69228dc016
sayedosama7 <sayedosama088@gmail.com>
2023-10-29 07:15:16 +0200

    New branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import HomePage from './Components/HomePage'
+import { GridLoader } from 'react-spinners';
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage/>
-      </div>
-    )
-  }
+function App() {
+  const [loading, setLoading] = useState([false]);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+  return (
+    <div>
+      {loading ?
+        <div className="loader">
+          <GridLoader color={'#663a2a'} size={50} speedMultiplier={1} />
+        </div> :
+        <HomePage />
+      }
+    </div>
+  )
 }
+
 
 export default App
